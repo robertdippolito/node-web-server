bec39f92666a285b3513d189dbce5a7270eece2c
Robert D'Ippolito <robertdippolito@Roberts-MacBook-Pro-2.local>
2018-09-20 21:13:25 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const hbs = require("hbs");
+const fs = require("fs");
+
+var app = express();
+hbs.registerPartials(__dirname + "/views/partials");
+
+app.set("view engine", "hbs");
+//__dirname finds the directory of the node-web-server file
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err) {
+      console.log('unable to append to server.log.')
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + "/public"));
+
+hbs.registerHelper("getCurrentYear", () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper("screamIt", (text) => {
+  return text.toUpperCase();
+})
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    myWelcome: 'Welcome to my home page',
+    });
+  })
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
+})
+
+app.get('/bad', (req, res) => {
+  res.send({
+    error: 'Unable to handle this request'
+  })
+})
+
+app.listen(3000, () => {
+  console.log("Server is up on port 3000");
+});
